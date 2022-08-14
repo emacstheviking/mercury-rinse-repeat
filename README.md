0c6bff77e71364346000e42117c3c60d674322d4
Sean Charles <objitsu@gmail.com>
2022-08-14 19:47:26 +0100

    Initial commit to the four winds

---
+++ README.md
@@ -0,0 +1,74 @@
+# Rinse, Repeat -- `rr`
+
+Rinse repeat. Having gotten fed up with the sheer amount of baggage that a lot
+of 'file watchers' come with (forget NPM, not going on my machine, ever) I
+decided that what I needed was something utterly simple, that doesn't grind
+the CPU and that just does what it says on the tin, I offer `rr`. Rinse,
+repeat.
+
+Once installed and on your OS path, all you do is:
+
+    $ rr "command-to-run" list-of-files-to-trigger-a-change
+
+The list can include wildcards, but I am relying on the operating system to
+expand that list prior to beginning execution i.e. the code itself does not do
+anything when given "*.c", the OS would have scanned the local folder and
+handed over all files ending with ".c" as the command line argument list.
+
+My particular use case was to work in conjunction with my own hand-rolled
+literate programming tool, `mlpt`, so all I needed was to look for changes to
+the Markdown source file and then trigger a rebuild of the Mercury sources in
+the file and also to regenerate the PDF documentation at the same time.
+
+`rr` does all of that for me so all I need to do is sit and think and work!
+
+
+# Requirements
+
+Obviously you need to have installed a working Mercury compiler, but also,
+your platform must have available the "C" function to sleep,
+
+    usleep() -- unistd.h
+
+If that's broken for you, I don't know what to suggest. I have seen a smart
+piece of code from Peter Wang where he used a socket recv call with an empty
+socket list and a timeout to do the same thing but can I find it again?
+
+
+# Using it
+
+I made it simple, all it does is use the first command line argument as the
+command that is to be run after any changes are detected in the remaining list
+of files given to it. It does NOT run the command initially, rather it sits
+and waits until the *modification time* (mtime to all you *nix heads) changes
+for any one of the files, then it executes the first command, then guess
+what... it does it all over again. It displays the name of the file that
+triggered the change of state from waiting to executing.
+
+To stop it, use CTRL+C on most platforms.
+
+Here are some examples just to make it clearer.
+
+
+## Working with a build system
+
+    $ rr "make && make install" *.c *.h
+
+This would be given a list of affected files in the current folder, and when
+any one of them changes, the command to make and and then install is given. If
+the command fails, the output goes to the standard output stream, as for all
+commands. It won't stop `rr` from running.
+
+
+## Using with a literate programming tool
+
+    $ rr "mlpt \"-U *:../gfx.m\"" gfx.m
+
+Note the double escaping of the strings. The `mlpt` command says that it
+should process the file called `gfx.m`, render out the code to the file called
+`../gfx.m` and start from the root element called "*".
+
+
+# Bugs / Suggestions
+
+GitHub has a great issue system.
---
+++ rr.m
@@ -0,0 +1,311 @@
+%-----------------------------------------------------------------------------%
+%
+% File: rr.m
+% Main author: Sean Charles
+% Date: Sat Aug 13 16:07:42 2022
+%
+% Rinse repeat. Having gotten fed up with the sheer amount of baggage that a
+% lot of 'file watchers' come with (forget NPM, not going on my machine ever)
+% I decided that what I needed was something utterly simple, that doesn't grind
+% the CPU (mine sleeps to yield) and that just does what it says on the tin, I
+% offer `rr`. Rinse, repeat.
+%
+% Once installed and on your OS path, all you do is:
+%
+%    $ rr "command-to-run" list-of-files-to-trigger-a-change
+%
+% The list can include wildcards, but I am relying on the operating system to
+% expand that list prior to beginning execution i.e. the code itself does not
+% do anything when given "*.c", the OS would have scanned the local folder and
+% handed over all files ending with ".c" as the command line argument list.
+%
+% My particular use case was to work in conjunction with my own hand-rolled
+% literate programming tool, `mlpt`, so all I needed was to look for changes to
+% the Markdown source file and then trigger a rebuild of the Mercury sources in
+% the file and also to regenerate the PDF documentation at the same time.
+%
+% rr does all of that for me so all I need to do is sit and think and work!
+%
+%-----------------------------------------------------------------------------%
+:- module rr.
+
+:- interface.
+:- import_module io.
+
+:- pred main(io::di, io::uo) is det.
+
+:- implementation.
+
+:- import_module bool.
+:- import_module calendar.
+:- import_module list.
+:- import_module maybe.
+:- import_module string.
+
+main(!IO) :-
+    io.command_line_arguments(Args, !IO),
+
+    ( if Args = [ Rinser | Files ],
+        list.is_not_empty(Files)
+    then
+        io.format("run: %s when changes in %s\n",
+            [s(Rinser), s(string(Files))], !IO),
+
+        statfiles(Files, Sres, !IO),
+        (
+            Sres = yes(Stats),
+            watch_files(Rinser, Files, Stats, !IO)
+        ;
+            Sres = no
+        )
+    else
+        true
+    ).
+
+%----------------------------------------------------------------------------%
+
+    % Watch a bunch of files for any changes to then.
+    % Stats is the initial set of statinfo() records, we ask for them again
+    % in the same order, and if any are changed, we update the list and return
+    % an indicator of a change.
+    %
+    % CTRL-C is the only way to kill this application now!
+    %
+:- pred watch_files(string::in, list(string)::in, list(statinfo)::in,
+    io::di, io::uo) is det.
+
+watch_files(Command, Files, Stats, !IO) :-
+    usleep(100000, !IO), % 0.1 sec, CPU respect!!
+    statfiles(Files, Sres, !IO),
+    (
+        Sres = yes(NewStats),
+        check_changes(Stats, NewStats, Changed, !IO),
+        ( if  Changed = yes then
+            io.format(">> %s", [s(Command)], !IO),
+            io.flush_output(!IO),
+            io.call_system(Command, _, !IO),
+            io.format(" *OK*\n", [], !IO),
+            %show_changes(Stats, NewStats, !IO),
+            watch_files(Command, Files, NewStats, !IO)
+        else
+            watch_files(Command, Files, Stats, !IO)
+        )
+    ;
+        Sres = no,
+        io.format("Ending, something went wrong!\n", [], !IO)
+    ).    
+
+%----------------------------------------------------------------------------%
+
+    % Check for change on 'mtime' only!
+    %
+:- pred check_changes(list(statinfo)::in, list(statinfo)::in, bool::out,
+    io::di, io::uo) is det.
+
+check_changes(Old, New, Changed, !IO) :-
+    Check = (pred(Pair::in, !.Ch::in, !:Ch::out, !.IO::di, !:IO::uo) is det :-
+        Pair0 = list.det_index0(Pair, 0),
+        Pair1 = list.det_index0(Pair, 1),
+        Mtime0 = si_mtime(Pair0),
+        Mtime1 = si_mtime(Pair1),
+        ( if Mtime0 = Mtime1 then
+            true
+        else
+            !:Ch = yes,
+            io.format(">> %s\n", [s(si_file(Pair0))], !IO)
+        )
+    ),
+    list.foldl2(Check, list.chunk(list.zip(Old, New), 2), no, Changed, !IO).
+
+%----------------------------------------------------------------------------%
+
+    % Display a statinfo() against its changed counterpart.
+
+:- pred show_changes(list(statinfo)::in, list(statinfo)::in,
+    io::di, io::uo) is det.
+
+show_changes(Old, New, !IO) :-
+    P = (pred(Pair::in, !.IO::di, !:IO::uo) is det :-
+        Pair0 = list.det_index0(Pair, 0),
+        Pair1 = list.det_index0(Pair, 1),
+        show_statinfo(Pair0, !IO),
+        show_statinfo(Pair1, !IO),
+        io.nl(!IO)
+    ),
+    list.foldl(P, list.chunk(list.zip(Old, New), 2), !IO).
+
+:- pred show_statinfo(statinfo::in, io::di, io::uo) is det.
+
+show_statinfo(S, !IO) :-
+    io.format("%s %s %s %s\n",
+        [s(si_file(S)),
+         s(string(si_atime(S))),
+         s(string(si_mtime(S))),
+         s(string(si_ctime(S)))
+        ], !IO).
+
+%----------------------------------------------------------------------------%
+
+    % Given a list of files, return a list stat structures.
+    %
+:- pred statfiles(list(string)::in, maybe(list(statinfo))::out,
+    io::di, io::uo) is det.
+
+statfiles(Files, Out, !IO) :-
+    P = (pred(F::in, !.OK::in, !:OK::out, !.IO::di, !:IO::uo) is det :-
+        filestat(F, Res, !IO),
+        ( if Res = ok(Stat) then
+            cons(Stat, !OK)
+        else
+            true
+        )
+    ),
+    list.foldl2(P, Files, [], Stats, !IO),
+    ( if list.same_length(Files, Stats) then
+        Out = yes(Stats)
+    else
+        Out = no
+    ).
+
+%----------------------------------------------------------------------------%
+%
+% FFI "C" Routines to stat() a file and usleep() for a while.
+%
+%----------------------------------------------------------------------------%
+
+    % A Statinfo to return something about our file.
+    %
+:- type statinfo
+    --->    statinfo(
+                si_file::string,
+                si_size::int,
+                si_atime::date,
+                si_mtime::date,
+                si_ctime::date
+            ).
+
+:- pragma foreign_decl("C", "
+    #include <sys/stat.h>
+    #include <stdio.h>
+    #include <time.h>
+    #include <unistd.h>
+").
+
+%----------------------------------------------------------------------------%
+
+    % Create an io.res error response.
+    %
+:- func make_error(int::in, string::in) = (io.res::out) is det.
+
+make_error(E, Why) = Out :-
+    ( if E = 0 then
+        Out = ok
+    else
+        Out = io.error(io.make_io_error(Why))
+    ).
+
+:- pragma foreign_export("C", make_error(in, in) = (out), "makeError").
+
+%----------------------------------------------------------------------------%
+
+    % Convert a bunch of integers to a calendar.date value.
+    %
+:- func to_date(int::in, int::in, int::in, int::in, int::in, int::in)
+    = (date::out) is det.
+
+to_date(Yr, Mo, Dy, Hr, Mi, Se) = Date :-
+    Month = calendar.det_int_to_month(Mo),
+    Date = calendar.det_init_date(Yr, Month, Dy, Hr, Mi, Se, 0).
+
+:- pragma foreign_export("C", to_date(in, in, in, in, in, in)
+    = (out), "toDate").
+
+
+%----------------------------------------------------------------------------%
+
+    % Make the io.res(statinfo) response given the values from the
+    % stat structure for the file.
+    %
+:- func make_response(string::in, int::in, date::in, date::in, date::in)
+    = (io.res(statinfo)::out) is det.
+
+make_response(File, Size, ATime, MTime, CTime)
+    = ok(statinfo(File, Size, ATime, MTime, CTime)).
+
+:- pragma foreign_export("C", make_response(in, in, in, in, in)
+    = (out), "statResponse").
+
+%----------------------------------------------------------------------------%
+
+    % The wrapper around "C" stat() to get file information.
+    %
+:- pred filestat(string::in, io.res(statinfo)::out, io::di, io::uo) is det.
+
+:- pragma foreign_proc(
+    "C", filestat(File::in, Error::out, _IO0::di, _IO::uo),
+    [   promise_pure
+    ,   thread_safe
+    ,   will_not_throw_exception
+    ,   will_not_modify_trail
+    ,   does_not_affect_liveness
+    ,   tabled_for_io
+    ],
+    "
+    struct stat buffer;
+    int         status;
+    status = stat(File, &buffer);
+
+    if (status) {
+        Error = makeError(errno, strerror(errno));
+    }
+    else {
+        struct tm atime, mtime, ctime;
+        localtime_r(&buffer.st_atime, &atime);
+        localtime_r(&buffer.st_mtime, &mtime);
+        localtime_r(&buffer.st_ctime, &ctime);
+
+        Error = statResponse(
+            File,
+            buffer.st_size,
+            toDate(
+                atime.tm_year, atime.tm_mon, atime.tm_mday,
+                atime.tm_hour, atime.tm_min, atime.tm_sec
+            ),
+            toDate(
+                mtime.tm_year, mtime.tm_mon, mtime.tm_mday,
+                mtime.tm_hour, mtime.tm_min, mtime.tm_sec
+            ),
+            toDate(
+                ctime.tm_year, ctime.tm_mon, ctime.tm_mday,
+                ctime.tm_hour, ctime.tm_min, ctime.tm_sec
+            )
+        );
+    }
+    "
+).
+
+%----------------------------------------------------------------------------%
+
+    % Sleep for N uSeconds.
+    % This is used to give the CPU a break between rounds of the scanning
+    % loop whilst looking for changes.
+    %
+:- pred usleep(int::in, io::di, io::uo) is det.
+
+:- pragma foreign_proc(
+    "C", usleep(MicroSecs::in, _IO0::di, _IO::uo),
+    [   promise_pure
+    ,   thread_safe
+    ,   will_not_throw_exception
+    ,   will_not_modify_trail
+    ,   does_not_affect_liveness
+    ,   tabled_for_io
+    ],
+    "
+    usleep(MicroSecs);
+    "
+).
+
+%----------------------------------------------------------------------------%
+:- end_module rr.
+%----------------------------------------------------------------------------%
